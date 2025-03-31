5502714796aa70c46913f82cc7ac76efa0c00bfc
Jacek Rudek <115008121+jacekrudek@users.noreply.github.com>
2025-03-31 22:26:23 +0200

    jpa repos config & example controller cleanup

--- papas-pizzeria/src/main/java/com/pizzeria/main/PapasPizzeriaApplication.java
+++ papas-pizzeria/src/main/java/com/pizzeria/main/PapasPizzeriaApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.pizzeria.controllers", "com.pizzeria.repos"})
 @EntityScan("com.pizzeria.entities")
+@EnableJpaRepositories("com.pizzeria.repos")
 public class PapasPizzeriaApplication {
 
 	public static void main(String[] args) {
