3013f9ade735f950bc4e286dc3f822a7b8609ab9
Jacek Rudek <115008121+jacekrudek@users.noreply.github.com>
2025-03-31 21:49:18 +0200

    entities package config & cleanup
    - deleted irrelevant example components
    - added scan for entity package

--- papas-pizzeria/src/main/java/com/pizzeria/main/PapasPizzeriaApplication.java
+++ papas-pizzeria/src/main/java/com/pizzeria/main/PapasPizzeriaApplication.java
@@ -2,10 +2,11 @@ package com.pizzeria.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.pizzeria.api", "com.pizzeria.controllers", "com.pizzeria.entities"})
+@EntityScan("com.pizzeria.entities")
 public class PapasPizzeriaApplication {
 
 	public static void main(String[] args) {
