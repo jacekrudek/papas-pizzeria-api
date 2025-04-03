6c1ecee60344e02ecacdd5f06235a62ed5e0b4c2
htrgr29 <115478511+htrgr29@users.noreply.github.com>
2025-04-03 23:29:40 +0200

    created client entity

--- papas-pizzeria/src/main/java/com/pizzeria/entities/Client.java
+++ papas-pizzeria/src/main/java/com/pizzeria/entities/Client.java
@@ -1,5 +1,16 @@
 package com.pizzeria.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "client")
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idc;
+	private String name;
+	private int tableNumber;
 }
