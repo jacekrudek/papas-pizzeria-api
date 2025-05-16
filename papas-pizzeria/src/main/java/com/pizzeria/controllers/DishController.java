0cef1bd57565effc448175922715bf0d2048e7dc
VX1-AEiI <114992080+VX1-AEiI@users.noreply.github.com>
2025-05-16 12:06:32 +0200

    Issue #17 Update DishController.java
    DELETE /dishes/{idd} request

--- papas-pizzeria/src/main/java/com/pizzeria/controllers/DishController.java
+++ papas-pizzeria/src/main/java/com/pizzeria/controllers/DishController.java
@@ -2,11 +2,14 @@ package com.pizzeria.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.pizzeria.entities.Dish;
 import com.pizzeria.repos.DishRepository;
@@ -28,5 +31,20 @@ public class DishController {
 	@GetMapping
 	public @ResponseBody Iterable<Dish> getDishes() {
 		return dishRepo.findAll();
+	}
+
+	@DeleteMapping("/{idd}")
+	public @ResponseBody String deleteDish(@PathVariable Integer idd) {
+
+		if (dishRepo.existsById(idd)) {
+			try {
+				dishRepo.deleteById(idd);
+			} catch (IllegalArgumentException e) {
+				return "Failed to delete dish, error code: " + e.getMessage();
+			}
+			return "Successfully deleted dish with id " + idd;
+		}
+		
+		return "Failed to delete dish, invalid ID";
 	}
 }
